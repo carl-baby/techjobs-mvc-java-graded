bddd782680abeb5ca89a5f27b639ef3e472a2f87
Carl <99614187+carl-baby@users.noreply.github.com>
2022-11-05 17:20:54 -0500

    task one complete after adding another piece of structure

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -3,7 +3,9 @@ package org.launchcode.techjobs.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -23,4 +25,11 @@ public class SearchController {
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
 
+//    @GetMapping(value = "results")
+//    @ResponseBody
+//    public class ResultsController {
+//        public String searching() {
+//            return "I'm Searching!";
+//        }
+//    }
 }
